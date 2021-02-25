d03fa2da269b061993654bdbaf232f5282bc6893
Sandheep <broadsword@Anton.local>
2021-02-26 01:37:38 +0530

    added adjacency matrix

--- hashCode.py
+++ hashCode.py
@@ -53,8 +53,62 @@ class Hash:
         return file, S, TI, NS, NC, BP, SI, CT 
 
 
+    # Function to convert adjacency
+    # list to adjacency matrix  
+    def convert(self, adj, V):
+
+        # Initialize a matrix
+        matrix = [[0 for j in range(V)] 
+                    for i in range(V)]   
+        for i in range(V):
+            for j in adj[i]:
+                matrix[i][j] = 1
+        return matrix
+
+    # Function to insert vertices 
+    # to adjacency list
+    def insert(self, adj, u, v):
+
+        # Insert a vertex v to vertex u
+        adj[u].append(v)
+        return
+
+    # Function to display adjacency list
+    def printList(self,adj, V):
+        
+        for i in range(V):
+            print(i, end = '')
+            
+            for j in adj[i]:
+                print(' --> ' + str(j), end = '')
+            print()
+        print()
+
+    # Function to display adjacency matrix
+    def printMatrix(self, adj, V):
+        
+        for i in range(V):
+            for j in range(V):
+                print(adj[i][j], end = ' ')
+                
+            print()
+
+    
+    def RessToFile(self, ress, filename="trash"):
+        ressToFile = []
+        ressToFile.append(str(len(ress)) + "\n")
+        for k in range(len(ress)):
+            ressToFile.append(str(k) + "\n")
+            res = ress[k]
+            ressToFile.append(str(len(res)) + "\n")
+            for re in res:
+                ressToFile.append(" ".join(str(item) for item in re) + "\n")
+        with open(filename, "w") as f:
+            f.writelines(ressToFile)
+        return ressToFile
+        
+
 if __name__ == "__main__":
-    hash = Hash()
 
     filenameA = "a.txt"
     # filenameB = "b.txt"
@@ -66,9 +120,33 @@ if __name__ == "__main__":
     filenames = [filenameA]
 
     for filename in filenames:
+        hash = Hash()
         print(filename)
         file, S, TI, NS, NC, BP, SI, CT = hash.DataIn(filename)
-        # menuSorted, menuScoreSorted, argSort = hash.Sort(menu)
-        # ress = hash.Crude(M, T2, T3, T4, menu)
-        # ressToFile = hash.ResToFile(ress, filename + ".out")
-        # print(ressToFile[0])
+
+
+
+
+        # adjacency matrix code
+        V = int(TI)
+        adjList = [[] for i in range(V)]
+
+        # Inserting edges
+        for i in range(NS):
+            hash.insert(adjList, int(SI[i][0]), int(SI[i][1]))
+            i+=1
+        # Display adjacency list
+        print("Adjacency List: ")
+        hash.printList(adjList, V)
+
+        # Function call which returns
+        # adjacency matrix after conversion
+        adjMatrix = hash.convert(adjList, V)
+
+        # Display adjacency matrix
+        print("Adjacency Matrix: ")
+        hash.printMatrix(adjMatrix, V)
+
+
+
+
